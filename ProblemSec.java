3295ed4295b5f153d98af350b7d6856892fbdae2
dev1ender <dev1ender@gmail.com>
2017-04-18 11:27:01 +0530

    core java 2,3
    core java assignment are uploaded with the both source and class file(executable file)

---
+++ ProblemSec.java
@@ -0,0 +1,16 @@
+public class ProblemSec
+{
+ public static void main (String []args)
+	{
+		int a =20;
+		int b= 10;
+                System.out.println("b = "+b);
+		System.out.println("a = "+a);
+		a = a+b;
+		b = a-b;
+		a = a-b;
+		System.out.println("b = "+b);
+		System.out.println("a = "+a);
+
+	}
+}
